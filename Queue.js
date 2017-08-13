279dd109c02220b3487bc0016a71c7e21647c54c
Michael <neradivx@lenta.ru>
2017-08-13 19:58:11 +0700

    Update Queue.js

--- Queue.js
+++ Queue.js
@@ -1,3 +1,6 @@
+/*
+http://neerc.ifmo.ru/wiki/index.php?title=%D0%9E%D1%87%D0%B5%D1%80%D0%B5%D0%B4%D1%8C
+*/
 function Queue() 
 {
 	this.queue = [];
@@ -34,3 +37,39 @@ queue.push(1);
 queue.push(2);
 queue.push(3);
 console.log(queue.pop());
+
+// Queue based on list
+function Node(value)
+{
+	this.value = value;
+	this.next = null;
+}
+
+function QueueOnList()
+{
+	this.head = null;
+	this.tail = null;
+}
+
+QueueOnList.prototype.push = function (val) {
+	var node = this.tail;
+	this.tail = new Node(val);
+	if (!this.head) {
+		this.head = this.tail;
+	} else {
+		node.next = this.tail;
+	}
+}
+
+QueueOnList.prototype.pop = function () {
+	var element = this.head;
+	this.head = this.head.next;
+	
+	return element;
+}
+
+var ob = new QueueOnList;
+ob.push(1);
+ob.push(2);
+ob.push(3);
+console.log(ob.pop);
